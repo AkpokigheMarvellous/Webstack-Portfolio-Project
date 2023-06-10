a52835568e18495a561c3020891c8193c5ae79ba
Akpokighe Marvellous O <m.ovigwe@yahoo.com>
2023-06-10 07:52:05 +0100

    editing navbar page

--- marvend/src/components/Navbar.js
+++ marvend/src/components/Navbar.js
@@ -30,7 +30,8 @@ const Navbar = () => {
     <div className={color ? "header header-bg" : 
     "header"}>
         <Link to="/">
-           <h1>Portfolio</h1>
+           <h1>MarvenD <br />
+            Portfolio</h1>
         </Link>
 
         <ul className={ click ? "nav-menu active" :
